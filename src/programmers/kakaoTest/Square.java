967ac0d2c34b7c55cb80353bbae7d3e6ea212eba
ahnzhx <modunaeggu@naver.com>
2020-04-24 20:55:18 +0900

    주석 수정

--- src/programmers/kakaoTest/Square.java
+++ src/programmers/kakaoTest/Square.java
@@ -3,7 +3,7 @@ package programmers.kakaoTest;
 /**
  * 멀쩡한 사각형
  * (최대공약수, 최소공배수 구하는 방법 기본으로 알아놓기)
- * (직각 사각형에서 대각선을 그었을 때, 멀쩡한 사각형 구하는 공식 : 가로 + 세로 -1 ,,, ㅅㅂ 이걸 어케알어!!)
+ * (직각 사각형에서 대각선을 그었을 때, 멀쩡한 사각형 구하는 공식 : 가로 + 세로 -1 ,,, )
  */
 public class Square {
     public long solution(int w,int h) {
