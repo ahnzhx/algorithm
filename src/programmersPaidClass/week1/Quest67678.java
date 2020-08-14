2c84bdd163f1576616464bfcf105cbbc7853b887
ahnzhx <modunaeggu@naver.com>
2020-08-14 22:08:43 +0900

    mokao algorithm - i did it so happy

--- src/programmersPaidClass/week1/Quest67678.java
+++ src/programmersPaidClass/week1/Quest67678.java
@@ -4,12 +4,14 @@ import javax.jnlp.IntegrationService;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Quest67678 {
     public int[] solution(int[] answers) {
         List<int[]> studentsAnswer = Arrays.asList(new int[]{1,2,3,4,5}, new int[]{2,1,2,3,2,4,2,5}, new int[]{3,3,1,1,2,2,4,4,5,5});
-        List<int[]> submittedStudentsAnswer = new ArrayList<>();
+        List<Integer> scoreBoard = new ArrayList<>();
 
+        // 1. 학생별로 점수를 계산한다.
         for(int[] studentAnswer : studentsAnswer){
             int[] answerSheet = new int[answers.length];
             int circuit = answers.length / studentAnswer.length;
@@ -24,32 +26,27 @@ public class Quest67678 {
                     al++;
                 }
             }
-
-            submittedStudentsAnswer.add(answerSheet);
-        }
-
-        List<Integer> studentsWhoGotHighestScore = new ArrayList<>();
-
-        for(int j =0 ; j < submittedStudentsAnswer.size() ; j++){
-            int score = 0;
-            for(int i = 0; i< answers.length; i++){
-                if(answers[i] == submittedStudentsAnswer.get(j)[i]){
-                    score ++;
-                }
+            int score =0;
+            for(int i = 0; i < answers.length ; i++){
+                if( answers[i] == answerSheet[i] )
+                   score++;
             }
-            studentsWhoGotHighestScore.add(score);
+            scoreBoard.add(score);
         }
 
+        // 2.최대 점수를 찾는다.
+        int max  = scoreBoard.stream().max(Integer::compareTo).get();
 
-        int max  = studentsWhoGotHighestScore.stream().max(Integer::compareTo).get();
-        studentsWhoGotHighestScore.removeIf(score -> (score != max));
-        studentsWhoGotHighestScore.stream().sorted();
-
-        int answer[] = new int[studentsWhoGotHighestScore.size()];
-        for(int p =0; p < studentsWhoGotHighestScore.size() ; p++){
-            answer[p] = p+1;
+        // 3.최대 점수와 같은 학생을 모은다.
+        List<Integer> answerList = new ArrayList<>();
+        for(int j = 0 ; j < scoreBoard.size() ; j ++){
+            if(scoreBoard.get(j) == max){
+                answerList.add(j+1);
+            }
         }
-        return answer;
+
+        // 4.그 학생들의 번호를 반환한다.
+        return answerList.stream().mapToInt(i -> i).toArray();
     }
 
     public static void main(String[] args) {
