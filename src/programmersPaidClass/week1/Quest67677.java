7ec25a5438847bf2c09eafc1a86d5937f82f9fa5
ahnzhx <modunaeggu@naver.com>
2020-08-14 22:18:24 +0900

    check binary one

--- src/programmersPaidClass/week1/Quest67677.java
+++ src/programmersPaidClass/week1/Quest67677.java
@@ -3,25 +3,37 @@ package programmersPaidClass.week1;
 public class Quest67677 {
     public int solution(int n) {
         int countOne = checkIfItHasOne(n);
+        int answer = n;
+        while(checkIfItHasOne(++answer) != countOne) ;
+        return answer;
 
-        int countOneOfNextBigInteger = 0;
-        int nextBigNum = n;
-        while(countOne != countOneOfNextBigInteger){
-            nextBigNum++;
-            countOneOfNextBigInteger = checkIfItHasOne(nextBigNum);
-        }
-        return nextBigNum;
+//        int countOneOfNextBigInteger = 0;
+//        int nextBigNum = n;
+//        while(countOne != countOneOfNextBigInteger){
+//            nextBigNum++;
+//            countOneOfNextBigInteger = checkIfItHasOne(nextBigNum);
+//        }
+//        return nextBigNum;
+
     }
 
     private int checkIfItHasOne (int checkingNumber){
-        int numberOfOne = 0;
-        String binaryN = Integer.toBinaryString(checkingNumber);
-        char[] charBinaryN  = binaryN.toCharArray();
-        for(char character : charBinaryN){
-            if(character == '1')
-                numberOfOne++;
+//        int numberOfOne = 0;
+//        String binaryN = Integer.toBinaryString(checkingNumber);
+//        char[] charBinaryN  = binaryN.toCharArray();
+//        for(char character : charBinaryN){
+//            if(character == '1')
+//                numberOfOne++;
+//        }
+//        return numberOfOne;
+
+        int n = checkingNumber;
+        int count =0;
+        while(n > 0){
+            if ((n & 1) == 1) count++;
+            n >>>= 1;
         }
-        return numberOfOne;
+        return count;
     }
 
     public static void main(String[] args) {
