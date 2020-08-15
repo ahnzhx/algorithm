fe5161c575b8070634d19291acad01eea39dbaf7
ahnzhx <modunaeggu@naver.com>
2020-08-15 14:53:03 +0900

    solve English Ending game

--- src/programmersPaidClass/week1/Quest67679.java
+++ src/programmersPaidClass/week1/Quest67679.java
@@ -1,52 +1,62 @@
 package programmersPaidClass.week1;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Quest67679 {
 
     public int solution(String dirs) {
         char[] dirsArray = dirs.toCharArray();
-        boolean[][] visited = new boolean[11][11];
-        int row = 5;
-        int col = 5;
+        boolean[][] visited = new boolean[100][100];
         int answer = 0;
 
-        for(char direction : dirsArray){
-            switch (direction){
-                case 'U':
-                    if(row-1 >= 0){
-                        if(!visited[--row][col]){
-                            visited[row][col] = true;
-                            answer++;
-                        }
-                    }
-                    break;
-                case 'D':
-                    if(row+1 >= 0 ){
-                        if(!visited[++row][col]){
-                            visited[row][col] = true;
-                            answer++;
-                        }
 
-                    }
-                    break;
-                case 'L':
-                    if( col-1 >= 0){
-                        if(!visited[row][--col]){
-                            visited[row][col] = true;
-                            answer++;
-                        }
+        // TODO 1.(00,00) 초기화
 
-                    }
-                    break;
-                case 'R':
-                    if(col+1 >= 0){
-                        if(!visited[row][++col]){
-                            visited[row][col] = true;
-                            answer++;
-                        }
-                    }
-                    break;
-            }
-        }
+
+        // TODO 2. U, D, L, R
+//        for(char direction : dirsArray){
+//            switch (direction){
+//                case 'U':
+//                    if( from + 1 <= 05){
+//                        if(!visited.get(from).contains(from+1)){
+//                            visited.get(from).add(++from);
+//                            answer++;
+//                        }
+//                    }
+//                    break;
+//                case 'D':
+//                    if(from -1 >= -05 ){
+//                        if(!visited.get(from).contains(from-1)){
+//                            visited.get(from).add(--from);
+//                            answer++;
+//                        }
+//
+//                    }
+//                    break;
+//                case 'L':
+//                    if( from-10 >= -50){
+//                        if(!visited.get(from).contains(from-10)){
+//                            from -= 10;
+//                            visited.get(from).add(from);
+//                            answer++;
+//                        }
+//
+//                    }
+//                    break;
+//                case 'R':
+//                    if(from +10 <= 50){
+//                        if(!visited.get(from).contains(from + 10)){
+//                            from +=10;
+//                            visited.get(from).add(from);
+//                            answer++;
+//                        }
+//                    }
+//                    break;
+//            }
+//        }
         return answer;
     }
 
---
+++ src/programmersPaidClass/week1/Quest67680.java
@@ -0,0 +1,42 @@
+package programmersPaidClass.week1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Quest67680 {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        Set<String> ending = new HashSet<>();
+
+        String wordLastLetter = words[0].substring(words[0].length() - 1);
+        ending.add(words[0]);
+        for(int i =1 ; i< words.length; i++){
+            if(!ending.contains(words[i]) && words[i].startsWith(wordLastLetter)){
+                ending.add(words[i]);
+            } else if(ending.contains(words[i]) || !words[i].startsWith(wordLastLetter)){
+                if((i+1) % n == 0){
+                    answer[0]= n;
+                    answer[1] = (i+1) / n;
+                }else{
+                    answer[0] = (i+1) % n;
+                    answer[1] = (i+1) / n + 1;
+                }
+                return answer;
+            }
+            wordLastLetter = words[i].substring(words[i].length()-1);
+        }
+        answer[0] = 0;
+        answer[1] = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] words = new String[]{"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+        System.out.println(new Quest67680().solution(3, words));
+
+//        String[] words = new String[]{"hello", "one", "even", "never", "now", "world", "draw"};
+//        System.out.println(new Quest67680().solution(2, words));
+
+    }
+}
