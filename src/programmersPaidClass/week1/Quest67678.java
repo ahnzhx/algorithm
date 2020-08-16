fa77ee8b3c113f597dd5058013eebd400e0d5755
ahnzhx <modunaeggu@naver.com>
2020-08-16 09:55:37 +0900

    length of visit

--- src/programmersPaidClass/week1/Quest67678.java
+++ src/programmersPaidClass/week1/Quest67678.java
@@ -1,36 +1,20 @@
 package programmersPaidClass.week1;
 
-import javax.jnlp.IntegrationService;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Quest67678 {
+    private Student student = new Student();
+
     public int[] solution(int[] answers) {
+
         List<int[]> studentsAnswer = Arrays.asList(new int[]{1,2,3,4,5}, new int[]{2,1,2,3,2,4,2,5}, new int[]{3,3,1,1,2,2,4,4,5,5});
         List<Integer> scoreBoard = new ArrayList<>();
 
         // 1. 학생별로 점수를 계산한다.
         for(int[] studentAnswer : studentsAnswer){
-            int[] answerSheet = new int[answers.length];
-            int circuit = answers.length / studentAnswer.length;
-            int al = 0;
-
-            for(int c = 0; c <= circuit; c++){
-                for(int sa = 0; sa< studentAnswer.length; sa++){
-                    if(al == answers.length){
-                        break;
-                    }
-                    answerSheet[al] = studentAnswer[sa];
-                    al++;
-                }
-            }
-            int score =0;
-            for(int i = 0; i < answers.length ; i++){
-                if( answers[i] == answerSheet[i] )
-                   score++;
-            }
+            int score = student.calculateScore(answers, studentAnswer);
             scoreBoard.add(score);
         }
 
@@ -39,11 +23,7 @@ public class Quest67678 {
 
         // 3.최대 점수와 같은 학생을 모은다.
         List<Integer> answerList = new ArrayList<>();
-        for(int j = 0 ; j < scoreBoard.size() ; j ++){
-            if(scoreBoard.get(j) == max){
-                answerList.add(j+1);
-            }
-        }
+        answerList = student.compareWithHighestScore(scoreBoard, answerList, max);
 
         // 4.그 학생들의 번호를 반환한다.
         return answerList.stream().mapToInt(i -> i).toArray();
--- src/programmersPaidClass/week1/Quest67679.java
+++ src/programmersPaidClass/week1/Quest67679.java
@@ -1,63 +1,98 @@
 package programmersPaidClass.week1;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 내가 못 푼 문제.. Point와 HistoryPoint 구현법을 알았다.
+ */
 public class Quest67679 {
+
+    private static final int MAX = 5;
+    private static final int MIN = -5;
+    private List<HistoryPoint> histories = new ArrayList<>();
+
+    class Point{
+        private int x;
+        private int y;
+
+        public Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class HistoryPoint {
+        private Point fromPoint;
+        private Point toPoint;
+
+        public HistoryPoint(int fromX, int fromY, int toX, int toY) {
+            this.fromPoint = new Point(fromX, fromY);
+            this.toPoint = new Point(toX, toY);
+        }
+
+        // 구현해줘야 할까?
+        @Override
+        public boolean equals(Object o) {
+            HistoryPoint other = (HistoryPoint) o;
+            return this.fromPoint.x == other.fromPoint.x
+                    && this.fromPoint.y == other.fromPoint.y
+                    && this.toPoint.x == other.toPoint.x
+                    && this.toPoint.y == other.toPoint.y;
+        }
+    }
+
 
     public int solution(String dirs) {
         char[] dirsArray = dirs.toCharArray();
-        boolean[][] visited = new boolean[100][100];
         int answer = 0;
-
-
-        // TODO 1.(00,00) 초기화
-
+        //시작점
+        Point startPoint = new Point(0,0);
 
         // TODO 2. U, D, L, R
-//        for(char direction : dirsArray){
-//            switch (direction){
-//                case 'U':
-//                    if( from + 1 <= 05){
-//                        if(!visited.get(from).contains(from+1)){
-//                            visited.get(from).add(++from);
-//                            answer++;
-//                        }
-//                    }
-//                    break;
-//                case 'D':
-//                    if(from -1 >= -05 ){
-//                        if(!visited.get(from).contains(from-1)){
-//                            visited.get(from).add(--from);
-//                            answer++;
-//                        }
-//
-//                    }
-//                    break;
-//                case 'L':
-//                    if( from-10 >= -50){
-//                        if(!visited.get(from).contains(from-10)){
-//                            from -= 10;
-//                            visited.get(from).add(from);
-//                            answer++;
-//                        }
-//
-//                    }
-//                    break;
-//                case 'R':
-//                    if(from +10 <= 50){
-//                        if(!visited.get(from).contains(from + 10)){
-//                            from +=10;
-//                            visited.get(from).add(from);
-//                            answer++;
-//                        }
-//                    }
-//                    break;
-//            }
-//        }
+        for(char direction : dirsArray){
+            int fromX = startPoint.x;
+            int fromY = startPoint.y;
+            int toX = fromX;
+            int toY = fromY;
+
+            switch (direction){
+                case 'U':
+                    toY++;
+                    break;
+                case 'D':
+                    toY--;
+                    break;
+                case 'L':
+                    toX--;
+                    break;
+                case 'R':
+                    toX++;
+                    break;
+            }
+
+            if(!isOut(toX, toY)){
+                if(isFirst(fromX, fromY, toX, toY)){
+                    histories.add(new HistoryPoint(fromX, fromY,toX, toY));
+                    histories.add(new HistoryPoint(toX, toY, fromX, fromY));
+                    answer++;
+                }
+                startPoint.x = toX;
+                startPoint.y = toY;
+            }
+        }
         return answer;
+    }
+
+    private boolean isOut(int toX, int toY){
+        if((toX > MAX || toX < MIN) || (toY > MAX || toY<MIN)){
+            return true;
+        }
+        return false;
+    }
+    private boolean isFirst(int fromX, int fromY, int toX, int toY){
+        return !histories.contains(new HistoryPoint(fromX, fromY, toX, toY));
     }
 
     public static void main(String[] args) {
