d27a2617a418d10420320f1ad194cdc473748fc9
ahnzhx <modunaeggu@naver.com>
2020-08-19 07:19:54 +0900

    set test class and refactor english ending game

--- src/programmersPaidClass/week1/Quest67680.java
+++ src/programmersPaidClass/week1/Quest67680.java
@@ -4,28 +4,23 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Quest67680 {
+
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
         Set<String> ending = new HashSet<>();
 
-        char wordLastLetter = getWordLastLetter(words[0]);
+        char wordLastLetter = words[0].charAt(words[0].length()-1);
         ending.add(words[0]);
 
-        // TODO: nested if 로 바꾸기
         for(int i =1 ; i< words.length; i++){
             if(!ending.contains(words[i]) && words[i].startsWith(wordLastLetter + "")){
                 ending.add(words[i]);
-            } else if(ending.contains(words[i]) || !words[i].startsWith(wordLastLetter + "")){
-                if((i+1) % n == 0){
-                    answer[0] = n;
-                    answer[1] = (i+1) / n;
-                }else{
-                    answer[0] = (i+1) % n;
-                    answer[1] = (i+1) / n + 1;
-                }
-                return answer;
+            } else if(ending.contains(words[i])){
+                return checkLoser(answer, n, i);
+            } else if(!words[i].startsWith(wordLastLetter + "")){
+                return checkLoser(answer, n, i);
             }
-            wordLastLetter = getWordLastLetter(words[i]);
+            wordLastLetter = words[i].charAt(words[i].length()-1);
         }
         answer[0] = 0;
         answer[1] = 0;
@@ -33,9 +28,6 @@ public class Quest67680 {
         return answer;
     }
 
-    private char getWordLastLetter(String word){
-        return word.toCharArray()[word.length()-1];
-    }
 
     private int[] checkLoser(int[] answer, int n, int i){
         if((i+1) % n == 0){
