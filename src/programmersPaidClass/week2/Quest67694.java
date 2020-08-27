79c17afa325a890714c845b210dda1c50b640985
ahnzhx <modunaeggu@naver.com>
2020-08-27 18:48:34 +0900

    solve disguise

--- src/programmersPaidClass/week2/Quest67694.java
+++ src/programmersPaidClass/week2/Quest67694.java
@@ -1,31 +1,22 @@
 package programmersPaidClass.week2;
 
-import java.util.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Quest67694 {
     public int solution(String[][] clothes) {
-        int answer = 0;
+        int answer = 1;
         Map<String, Integer> combination = new HashMap<>();
 
         for(int i = 0; i < clothes.length; i++){
-            combination.computeIfPresent(clothes[i][1], (key, val) -> val+1 );
-            combination.computeIfAbsent(clothes[i][1], v -> 1);
-        }
-        int keySetSize = combination.keySet().size();
-        // TODO. 몇 개씩 뽑을건지 (1 ~ 4)
-        for(int i =1 ; i <= keySetSize; i++){
-            for (String s : combination.keySet()) {
-                answer += combination.get(s);
-            }
-
+            combination.put(clothes[i][1], combination.getOrDefault(clothes[i][1], 0) + 1);
         }
 
+        for (String s : combination.keySet()) {
+            answer *= combination.get(s) + 1;
+        }
 
-
-
-
-
-
-        return 0;
+        return answer - 1;
     }
 }
