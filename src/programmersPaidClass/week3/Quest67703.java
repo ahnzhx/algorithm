4ab149a721814339948ccec3a2f1cf9237e8f499
ahnzhx <modunaeggu@naver.com>
2020-09-05 17:55:09 +0900

    solving game map

--- src/programmersPaidClass/week3/Quest67703.java
+++ src/programmersPaidClass/week3/Quest67703.java
@@ -1,8 +1,6 @@
 package programmersPaidClass.week3;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Quest67703 {
     private static final int east = +1;
@@ -10,39 +8,43 @@ public class Quest67703 {
     private static final int north = -1;
     private static final int south = 1;
 
-    private Stack<Location> locationQ = new Stack<>();
-    private boolean[][] visited ;
+
+
     public int solution(int[][] maps) {
-        int answer = 0;
-        Location robotLoc = new Location(0,0);
-        visited = new boolean[maps.length][maps[0].length];
-        List<Integer> answerList = new ArrayList<>();
-        locationQ.add(robotLoc);
-        visited[0][0] = true;
+        int n = maps.length;
+        int m = maps[0].length;
 
-        while(!locationQ.isEmpty()){
-            robotLoc = locationQ.pop();
-            if(robotLoc.isEastOk(maps)){
-                answer++;
-            }
-            if(robotLoc.isWestOk(maps)){
-                answer++;
-            }
-            if(robotLoc.isNorthOk(maps)){
-                answer++;
-            }
-            if(robotLoc.isSouthOk(maps)){
-                answer++;
+        Stack<Location> stack = new Stack<>();
+        // 방문 좌표와 거리
+        Map<Location, Integer> visited = new HashMap<>();
+
+        // 시작점, 도착점
+        Location now = new Location(0,0);
+        Location last = new Location(0,0);
+
+        visited.put(now, 1);
+        stack.push(now);
+
+        while(!stack.isEmpty()){
+            now = stack.pop();
+
+            for(int i =0 ; i < 4; i++){
+                Location next = now.moveTo(i);
+                // 맵을 벗어난 경우 / 이미 방문한 좌표일 경우 / 방문할 좌표가 벽인 경우
+                if(next.isValid(n, m) ){
+
+                }
             }
 
-
-            if(robotLoc.isArrived(maps)){
-                answerList.add(answer);
+            if(now.isArrived(maps)){
+                //answerList.add();
             }
         }
-
-
-        return answer;
+        Collections.sort(answerList);
+        if(answerList.isEmpty()){
+            return -1;
+        }
+        return answerList.get(0);
     }
 
     class Location{
@@ -54,70 +56,16 @@ public class Quest67703 {
             this.y = y;
         }
 
-        public Location moveLocation(int x, int y){
-            return new Location(x,y);
-        }
-
-        public boolean isEastOk(int[][] maps){
-            if(y + east < maps[0].length ){
-                if(maps[x][y + east] != 0 ){
-                    if(!visited[x][y + east]){
-                        locationQ.add(moveLocation(x, y+east));
-                        visited[x][y + east] = true;
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-
-        public boolean isWestOk(int[][] maps){
-            if(y + west >= 0){
-                if(maps[x][y + west] != 0 ){
-                    if(!visited[x][y + west]){
-                        visited[x][y + west] = true;
-                        locationQ.add(moveLocation(x, y+west));
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-
-        public boolean isNorthOk(int[][] maps){
-            if( x + north >= 0 ){
-                if(maps[x + north][y] != 0){
-                    if(!visited[x + north][y]){
-                        visited[x + north][y] = true;
-                        locationQ.add(moveLocation(x + north, y));
-                        return true;
-                    }
-
-                }
-            }
-            return false;
-        }
-
-        public boolean isSouthOk(int[][] maps){
-            if(x + south < maps.length){
-                if(maps[x + south][y] != 0){
-                    if(!visited[x + south][y]){
-                        visited[x + south][y] = true;
-                        locationQ.add(moveLocation(x + south, y));
-                        return true;
-                     }
-                }
-            }
-            return false;
+        public Location moveTo(int dir){
+            int[] dx = {-1, 1, 0, 0}, dy = {0,0, -1, 1};
+            return new Location(x + dx[dir], y + dy[dir]);
         }
 
         public boolean isArrived(int[][] maps){
-            if(x == maps[0].length && y == maps.length)
+            if(x == maps[0].length-1 && y == maps.length-1)
                 return true;
             return false;
 
         }
     }
-
-
 }
