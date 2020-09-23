2ed2bbf5e8b99f4af4bb561a82f2f1501a67a950
ahnzhx <modunaeggu@naver.com>
2020-09-23 10:34:55 +0900

    greedy

--- src/programmersPaidClass/week4/Quest67707.java
+++ src/programmersPaidClass/week4/Quest67707.java
@@ -1,9 +1,29 @@
 package programmersPaidClass.week4;
 
+import java.util.Arrays;
+
 public class Quest67707 {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
+        int[] student = new int[n];
+        Arrays.fill(student, 1 );
 
+        for(int i : lost) student[i-1] -=1;
+        for(int i : reserve) student[i-1] +=1;
+
+        for(int i = 0; i < n; i++){
+            if(student[i] <= 1 ){ continue; }
+
+            if(i> 0 && student[i-1] < 1){
+                student[i-1] += 1;
+                student[i] -= 1;
+            }else if(i < n-1 && student[i+1] < 1){
+                student[i+1] += 1;
+                student[i] -= 1;
+            }
+        }
+
+        int answer = 0;
+        for(int i =0 ; i < n ; i++){ answer += student[i] >= 1 ? 1: 0; }
         return answer;
     }
 }
--- src/test/programmersPaidClass/week4/Quest67707Test.java
+++ src/test/programmersPaidClass/week4/Quest67707Test.java
@@ -8,14 +8,16 @@ public class Quest67707Test extends TestCase {
         Quest67707 quest67707 = new Quest67707();
         int lost[] = new int[]{2,4};
         int lost2[] = new int[]{3};
+        int lost3[] = new int[]{2,3};
 
         int reverse[] = new int[]{1,3,5};
         int reverse2[] = new int[]{3};
         int reverse3[] = new int[]{1};
 
+
         assertEquals(5, quest67707.solution(5, lost, reverse));
         assertEquals(4, quest67707.solution(5, lost, reverse2));
-        assertEquals(2, quest67707.solution(5, lost2, reverse3));
+        assertEquals(2, quest67707.solution(3, lost3, reverse3));
 
     }
 
