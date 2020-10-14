0e59162982141122c1ee9072db05eaab3c543ee7
ahnzhx <modunaeggu@naver.com>
2020-10-15 07:47:36 +0900

    daeho meoho

--- src/baekjoon/Backjoon14496.java
+++ src/baekjoon/Backjoon14496.java
@@ -1,12 +1,45 @@
 package baekjoon;
 
+import java.util.List;
 import java.util.Map;
 
 public class Backjoon14496 {
+    static class Pair{
+        private int start;
+        private int end;
 
-    //TODO: 푸는중
-    public int solution(Map<Integer, Integer> pairMap, int start, int goal){
-        pairMap.get(start);
-        return 0;
+        public int getStart() {
+            return start;
+        }
+
+        public int getEnd() {
+            return end;
+        }
+
+        public Pair(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public int solution(Map<Integer, List<Integer>> pairMap, int start, int goal){
+        List<Integer> endList = pairMap.get(start);
+        int min = Integer.MAX_VALUE;
+        for(Integer end : endList){
+            List<Integer> nextList = pairMap.get(end);
+            int comparingNum = 1;
+            for(Integer next : nextList){
+                comparingNum += 1;
+                if(next == goal){
+                    comparingNum += 1;
+                    if(comparingNum < min){
+                        min = comparingNum;
+                    }
+                }
+
+            }
+        }
+        if (min == Integer.MAX_VALUE) return -1;
+        return min;
     }
 }
--- src/test/baekjoon/Backjoon14496Test.java
+++ src/test/baekjoon/Backjoon14496Test.java
@@ -10,14 +10,27 @@ import java.util.Map;
 public class Backjoon14496Test extends TestCase {
     public void test(){
         Backjoon14496 backjoon14496 = new Backjoon14496();
-        Map<Integer, Integer> pairMap = new HashMap<>();
-        pairMap.put(4,4);
-        pairMap.put(1,3);
-        pairMap.put(1,4);
-        pairMap.put(3,2);
-        pairMap.put(4,2);
+        Map<Integer, List<Integer>> pairMap = new HashMap<>();
+        List<Backjoon14496.Pair> pairList = new ArrayList<>();
+        pairList.add(new Backjoon14496.Pair(4,4));
+        pairList.add(new Backjoon14496.Pair(1,3));
+        pairList.add(new Backjoon14496.Pair(1,4));
+        pairList.add(new Backjoon14496.Pair(3,2));
+        pairList.add(new Backjoon14496.Pair(4,2));
 
-        assertEquals(2, backjoon14496.solution(pairMap,1,2));
+        List<Integer> endList;
+        for(Backjoon14496.Pair pair : pairList){
+            endList = pairMap.get(pair.getStart());
+            if(endList == null ){
+                endList = new ArrayList<>();
+            }
+            endList.add(pair.getEnd());
+            pairMap.put(pair.getStart(), endList);
+
+        }
+
+        assertEquals(2, backjoon14496.solution(pairMap, 1,2));
+
     }
 
 }
