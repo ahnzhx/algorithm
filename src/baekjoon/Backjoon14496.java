dd704eb829745748766560f016149934a8d2910a
Sohyun Ahn <modunaeggu@naver.com>
2020-10-15 16:50:40 +0900

    Update Backjoon14496.java

--- src/baekjoon/Backjoon14496.java
+++ src/baekjoon/Backjoon14496.java
@@ -22,16 +22,20 @@ public class Backjoon14496 {
         }
     }
 
-    public int solution(Map<Integer, List<Integer>> pairMap, int start, int goal){
+   public static int solution(Map<Integer, List<Integer>> pairMap, int start, int goal){
         List<Integer> endList = pairMap.get(start);
         int min = Integer.MAX_VALUE;
+        
         for(Integer end : endList){
+        	if(endList.contains(goal)) {
+            	min = 1;
+            	return min;
+            }
             List<Integer> nextList = pairMap.get(end);
             int comparingNum = 1;
             for(Integer next : nextList){
                 comparingNum += 1;
                 if(next == goal){
-                    comparingNum += 1;
                     if(comparingNum < min){
                         min = comparingNum;
                     }
@@ -41,5 +45,35 @@ public class Backjoon14496 {
         }
         if (min == Integer.MAX_VALUE) return -1;
         return min;
+	}
+	
+	public static void main(String[] args) {
+	     Map<Integer, List<Integer>> pairMap = new HashMap<>();
+	     List<Pair> pairList = new ArrayList<>();
+	     
+	     Scanner sc = new Scanner(System.in);
+	     int start = sc.nextInt();
+	     int goal = sc.nextInt();
+	     
+	     int first = sc.nextInt();
+	     int next = sc.nextInt();
+	     
+	     pairList.add(new Pair(first, next));
+	     
+	     sc.close();
+	        	
+        List<Integer> endList;
+        for(Pair pair : pairList){
+            endList = pairMap.get(pair.getStart());
+            if(endList == null ){
+                endList = new ArrayList<>();
+            }
+            endList.add(pair.getEnd());
+            pairMap.put(pair.getStart(), endList);
+
+        }
+
+        System.out.println(solution(pairMap, first, goal));
+
     }
 }
