344746fbd10906c58acedb378ab76cb7b0a68315
ahnzhx <modunaeggu@naver.com>
2020-10-21 06:58:21 +0900

    algo study

---
+++ src/baekjoon/Baekjoon2529.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.util.*;
+
+public class Baekjoon2529 {
+
+    public static void main(String[] args) {
+        int[] numb = new int[]{0,1,2,3,4,5,6,7,8,9};
+        boolean[] chk = new boolean[10];
+        String[] ineqArr = new String[10];
+
+        Scanner scanner = new Scanner(System.in);
+        int ineqSignNum = scanner.nextInt();
+        for(int i =0 ; i< ineqSignNum ; i++){
+            ineqArr[i] = scanner.next();
+        }
+
+        // max 값 구할 때
+        for(int ){
+
+        }
+        // min 값 구할 때
+
+    }
+}
