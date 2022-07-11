7e6162e276cb941063a542555b4d5badbb9be5e8
ahnzhx <modunaeggu@naver.com>
2022-07-11 17:32:09 +0900

    finally solved edit distance

--- src/leetCode/EditDistance.java
+++ src/leetCode/EditDistance.java
@@ -1,58 +1,34 @@
 package leetCode;
 
 public class EditDistance {
-    static int ret = 0;
-    static int count = 0;
+
     public static int minDistance(String word1, String word2) {
-        int index = 0;
-        if (word2.length() == index || word1.length() == index) {
-            ret = Math.max(word1.length(), word2.length());
-        }else{
-            common(word1, word2);
-        }
+        int[][] dp = new int[word1.length()+1][word2.length()+1];
 
-        return ret;
-    }
-
-    public static void common(String word1, String word2){
-        if(word1.length() > 0 || word2.length() > 0){
-
-            for(int i = 0; i < word1.length(); i++){
-                if(word2.length() > i ){
-                    //todo execution 에 e가 2개 있을때 결과값 어떻게 나오는지 확인하기
-                    int includedCharInWord2 = word2.indexOf(word1.charAt(i));
-                    if( includedCharInWord2 > -1){
-                        //알파벳 위치와 알파벳이 동일한 경우
-                        if(includedCharInWord2 == i) {
-                            count++;
-//                            ret += Math.max(i, includedCharInWord2);
-                            common(word1.substring(0, i), word2.substring(0, includedCharInWord2));
-                            common(word1.substring(i+1), word2.substring(includedCharInWord2+1));
-                            break;
-                        }else if(includedCharInWord2 != i && count > 0){
-                            ret += Math.max(i, includedCharInWord2);
-                            common(word1.substring(i+1), word2.substring(includedCharInWord2+1));
-                        }
-                    }else if(count > 0){
-                        ret += Math.max(word1.length(), word2.length());
-
-                        break;
+        for(int i = 0; i <= word1.length(); i ++){
+            for(int j = 0; j <= word2.length(); j++){
+                if(i == 0){
+                    dp[i][j] = j;
+                }
+                else if(j == 0){
+                    dp[i][j] = i;
+                } else {
+                    if(word1.charAt(i-1) != word2.charAt(j-1)){
+                        int tempMin = Math.min(dp[i-1][j], dp[i][j-1]);
+                        dp[i][j] = Math.min(tempMin, dp[i-1][j-1])+1;
+                    }else{
+                        dp[i][j] = dp[i-1][j-1];
                     }
-//                    break;
-                }else{
-                    ret += Math.max(word1.length(), word2.length());
-
-                    break;
                 }
             }
         }
-
+        return dp[word1.length()][word2.length()];
     }
 
 
     public static void main(String[] args) {
-//        String word1 = "horse";
-//        String word2 = "ros";
+        String word1 = "horse";
+        String word2 = "ros";
 //        String word1 = "sea";
 //        String word2 = "eat";
 //        String word1 = "intention";
@@ -61,8 +37,8 @@ public class EditDistance {
 //        String word2 = "a";
 //       String word1 = "mart";
 //        String word2 = "karma";
-        String word1 = "plasma";
-        String word2 = "altruism";
+//        String word1 = "plasma";
+//        String word2 = "altruism";
         //          String word1 = "b";
 //        String word2 = "a";
 //          String word1 = "a";
