09faf020ed3de8addea290665e01d8189d5c18c7
ahnzhx <modunaeggu@naver.com>
2023-04-03 12:39:50 -0400

    best time to sell and buy second

---
+++ algo_2/src_test/BestTimetoBuyAndSellStockTwoTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+class BestTimetoBuyAndSellStockTwoTest {
+    @Test
+    public void test(){
+        BestTimetoBuyAndSellStockTwo btbss = new BestTimetoBuyAndSellStockTwo();
+//        assertEquals(7, btbss.maxProfit(new int[]{7,1,5,3,6,4}));
+//        assertEquals(4, btbss.maxProfit(new int[]{1,2,3,4,5}));
+//        assertEquals(0, btbss.maxProfit(new int[]{7,6,4,3,1}));
+//        assertEquals(7, btbss.maxProfit(new int[]{3,2,6,5,0,3}));
+    }
+
+}
